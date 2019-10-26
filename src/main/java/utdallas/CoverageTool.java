33d9136ba5b5cf38dd40797d0dcf8417a528a9a9
Yugesh Taksari <yugesh.taksari@utdallas.edu>
2019-10-26 16:23:54 -0500

    methodTransformVisitor update

--- src/main/java/utdallas/CoverageTool.java
+++ src/main/java/utdallas/CoverageTool.java
@@ -1,21 +1,35 @@
 package utdallas;
 import java.util.HashMap;
-//import java.util.HashSet;
+import java.util.HashSet;
+import java.util.Arrays;
 import java.util.LinkedHashSet;
-//import java.util.Set;
+import java.util.Set;
 
 public class CoverageTool {
-    public static HashMap<String, HashMap<String, LinkedHashSet<Integer>>> testSuite;
-    public static HashMap<String, LinkedHashSet<Integer>> testCase;
+
+    public static HashMap<String, HashSet> linesCovered;
+    public static HashMap<String, HashMap<String, HashSet>> testCases;
     public static String testName;
 
-    public static void visitLine(String className, Integer line){
-        if(testCase == null || className == null) return;
-        LinkedHashSet<Integer> set = testCase.get(className);
-        if(set == null) {
-            set = new LinkedHashSet<Integer>();
+    public static void addCoveredLine(String name, Integer line) {
+
+        // if the the lines covered is empty
+        if (linesCovered == null) {
+            System.out.println("This is line " + name + "| " + line);
+            return;
         }
-        set.add(line);
-        testCase.put(className, set);
+
+        HashSet covered_lines = linesCovered.get(name);
+        // If linesCovered has no values for the class name
+        if (covered_lines == null) {
+            int[] new_set = { line };
+            System.out.println("This is line " + name + "| " + line);
+            linesCovered.put(name, new HashSet<>(Arrays.asList(new_set)));
+        }
+
+        else {
+            System.out.println("This is the line " + name + "| " + line);
+            covered_lines.add(line);
+        }
     }
 }
